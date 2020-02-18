56624728ce76bf7daf111c2ed1f01c543b6beea4
Tommy Grabowski <tomaarongrabowski@gmail.com>
2020-02-18 16:08:09 -0500

    Twenty-sixth commit
    Web page updated to Episode 6

--- Website/JS/jscode.js
+++ Website/JS/jscode.js
@@ -1,8 +1,31 @@
+//My HTML is different than the HTML from the tutorial so I'm using a different selector.
+//Sytax: $(".class > child");
+
+//First, I selected the "content-box-content" class, then I used '>' to say I want to use a child 
+//element of the "content-box-content" class.
+
+//I am using ".toggle()" instead of ".hide()" so that the button toggles between 
+//.hide() and .show() when clicked
+
+//Also, I added more buttons for extra practice
+
+
+
 $(document).ready(function(){
-    $("button").click(function() {
-        $(".content-box-content > #left p:first").hide();       //My HTML is different than the HTML from the tutorial
-    })                                                          //so I'm using a different selector.
-                                                                //Sytax: $(".class > child");
-});                                                             //First, I selected the "content-box-content" class,
-                                                                //then selected the first div, which is a child of
-                                                                //"content-box-content"
+    $("#even").click(function() {
+        $(".content-box-content > div:even").toggle();          //Toggles even divs
+    })
+    
+    $("#odd").click(function() {
+        $(".content-box-content > div:odd").toggle();           //Toggles odd divs
+    })
+
+    $("#all").click(function() {
+        $(".content-box-content > div").toggle();               //Toggles all divs
+    })
+
+    $("#cen").click(function() {
+        $("p:contains('center')").toggle();                     //Toggles paragraphs containing the word 'center'
+    })
+                                                                
+});
