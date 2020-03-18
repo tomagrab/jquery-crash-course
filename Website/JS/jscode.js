d6189a7810785dc6a6a71c9234e302d453a18f26
Tommy Grabowski <tomaarongrabowski@gmail.com>
2020-03-18 16:05:42 -0400

    Thirty-third commit

--- Website/JS/jscode.js
+++ Website/JS/jscode.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     $("#test1").click(function() {
-        $("#left p:first").toggleClass("para");
+        $("#right").fadeIn(2500);
+    });
+    $("#test2").click(function() {
+        $("#right").fadeOut(2500);
+    });
+    $("#test3").click(function() {
+        $("").fadeTo("para");
     });
     
+    var width = $(".content-box").css("width");
+
+    $(".content-box").animate(width);
+
 })
